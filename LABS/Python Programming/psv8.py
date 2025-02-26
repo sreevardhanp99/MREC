b0c3c79e77d24862b7a5b9d57258287586b38f3d
Sree Vardhan Puligilla <sreevardhan.puligilla999@gmail.com>
2025-02-26 23:09:49 +0530

    Add files via upload

---
+++ LABS/Python Programming/psv8.py
@@ -0,0 +1,4 @@
+import webbrowser 
+webbrowser.open("https://support.microsoft.com/en-us/windows/find-settings-in-windows-10-6ffbef87-e633-45ac-a1e8-b7a834578ac6#:~:text=Find%20your%20Windows%20settings%20in,what%20you're%20looking%20for.")
+webbrowser.open_new("http://mrec.ac.in")
+webbrowser.open_new_tab("https://blogs.helsinki.fi/students-digital-skills/1-introduction-to-the-use-of-computers/1-1-computer-functionality/operating-system-settings/")
