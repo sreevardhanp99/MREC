259456ca6ed5061af97eae1f9c6246dca1480909
Sree Vardhan Puligilla <sreevardhan.puligilla999@gmail.com>
2025-04-13 12:24:44 +0530

    Add files via upload

---
+++ LABS/Python Programming/psv60 multilevel_inheritance.py
@@ -0,0 +1,14 @@
+class a:
+    x=int(input("enter the number:"))
+    y=int(input("enter yhe number:"))
+    z=x+y
+    v=x-y
+    def add(self):
+        
+        print(self.z)
+class b(a):
+    def sub(self):
+        print(s.v)
+s=b()
+s.add()
+s.sub()
